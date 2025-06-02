6eecf4ca9b75166a30159e83d577c0f1f2b0e09e
dima dubinin <drakingin@gmail.com>
2025-06-03 02:17:42 +0300

    Update searchNewChapters.py

--- scripts/searchNewChapters.py
+++ scripts/searchNewChapters.py
@@ -138,7 +138,8 @@ if __name__ == "__main__":
                         online_str = "❌ error" if online_chapter is None else online_chapter
                         print(f"✅ {site} - {folder}: No new chapter (Local: {local_chapter}, Online: {online_str})")
 
-            print("--------------------------------------------------------------------------------------")
+            print("_" * 86)
+
 
 
     if not new_found:
